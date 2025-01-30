c8dddf6056565d5d46bfc39b349cd7adff374acc
rickpie <P.cadoret@hotmail.fr>
2025-01-30 17:07:42 +0100

    fix js

--- js/script.js
+++ js/script.js
@@ -10,12 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Header container trouvé:", headerContainer);
     console.log("Footer container trouvé:", footerContainer);
 
-    // Fonction pour ajuster le chemin relatif des liens
-    function adjustLinkPath(path) {
-        // On utilise simplement le chemin relatif sans préfixe
-        return path;
-    }
-
     // Charger la navbar dans le header
     if (headerContainer) {
         console.log("Chargement du header...");
@@ -23,21 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
         <nav class="navbar">
             <div class="container">
                 <!-- Lien vers la racine du site -->
-                <a class="navbar-brand" href="./index.html">Le fumoir de Pierrick</a>
+                <a class="navbar-brand" href="/Fumoir_vitrine/index.html">Le fumoir de Pierrick</a>
                 <div class="navbar-menu">
                     <ul class="navbar-links">
                         <!-- Lien vers la page d'accueil -->
-                        <li><a href="./index.html">Accueil</a></li>
-                        <!-- On utilise des liens relatifs qui sont calculés à partir du dossier actuel -->
-                        <li><a href="./home_menu/conseils/conseil_index.html">Conseils</a></li>
-                        <li><a href="./home_menu/fumoir/fumoir_index.html">Fumoir</a></li>
-                        <li><a href="./home_menu/recettes/recettes_index.html">Recettes</a></li>
+                        <li><a href="/Fumoir_vitrine/index.html">Accueil</a></li>
+                        <!-- Liens vers les pages internes en chemin relatif -->
+                        <li><a href="/Fumoir_vitrine/home_menu/conseils/conseil_index.html">Conseils</a></li>
+                        <li><a href="/Fumoir_vitrine/home_menu/fumoir/fumoir_index.html">Fumoir</a></li>
+                        <li><a href="/Fumoir_vitrine/home_menu/recettes/recettes_index.html">Recettes</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <section id="banniere">
-            <img src="./images/banniere.jpg" alt="Bannière" class="img-banniere">
+            <img src="/Fumoir_vitrine/images/banniere.jpg" alt="Bannière" class="img-banniere">
         </section>`;
     } else {
         console.error("Erreur : Header container non trouvé.");
