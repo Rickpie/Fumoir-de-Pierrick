83a9af2a26328a94c43591db990430ada02be0e0
rickpie <P.cadoret@hotmail.fr>
2025-01-30 12:45:18 +0100

    test css homepage git

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
-// loadHeaderFooter.js
+fetch('includes/header.html')
+  .then(response => response.text())
+  .then(data => {
+    console.log("Header chargé avec succès !");
+    document.getElementById('header-container').innerHTML = data;
+  })
+  .catch(error => console.log('Erreur de chargement du header:', error));
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Charger dynamiquement le header (qui inclut aussi la navbar et la bannière)
-  fetch('/Fumoir_vitrine/includes/header.html')
-    .then(response => response.text())
-    .then(data => {
-      console.log("Header chargé avec succès !");
-      document.getElementById('header-container').innerHTML = data;
-    })
-    .catch(error => console.log('Erreur de chargement du header:', error));
-
-  // Charger dynamiquement le footer
-  fetch('/Fumoir_vitrine/includes/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      // Injecter le footer à la fin du body
-      document.body.insertAdjacentHTML('beforeend', data);
-    })
-    .catch(error => console.log('Erreur de chargement du footer:', error));
-});
+fetch('includes/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.body.insertAdjacentHTML('beforeend', data);
+  })
+  .catch(error => console.log('Erreur de chargement du footer:', error));
