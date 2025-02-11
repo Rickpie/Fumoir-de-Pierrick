e0bb87980bf54da72e782b077d5aefb28f56134b
rickpie <P.cadoret@hotmail.fr>
2025-02-11 13:42:23 +0100

    test fix Js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Détecte le dossier racine du projet sur GitHub Pages ou en local
+  let paths = window.location.pathname.split("/").filter((path) => path !== "");
+  let repoName =
+    paths.length > 0 && window.location.hostname.includes("github.io")
+      ? `/${paths[0]}/`
+      : "/";
+
   // Charger le header depuis la racine du site
-  fetch("/header/header.html")
+  fetch(`${repoName}header/header.html`)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header").innerHTML = data;
+
+      // Une fois le header chargé, corriger les liens
+      let navLinks = document.querySelectorAll(
+        "#header .navbar-links a, #header .navbar-brand"
+      );
+      navLinks.forEach((link) => {
+        let href = link.getAttribute("href");
+        if (href && !href.startsWith("http") && !href.startsWith("#")) {
+          // Préfixe avec le chemin absolu basé sur le repo
+          link.setAttribute("href", repoName + href);
+        }
+      });
+
+      // Corriger l'image de la bannière si nécessaire
+      let bannerImg = document.querySelector("#header #banniere img");
+      if (bannerImg) {
+        let src = bannerImg.getAttribute("src");
+        if (src && !src.startsWith("http")) {
+          bannerImg.setAttribute("src", repoName + src);
+        }
+      }
 
       // Menu hamburger pour les petits écrans
       const menuToggle = document.getElementById('menu-toggle');
@@ -15,34 +43,13 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         });
       }
-
-      // Corriger les liens dans le header pour utiliser des chemins absolus
-      let navLinks = document.querySelectorAll(
-        "#header .navbar-links a, #header .navbar-brand"
-      );
-      navLinks.forEach((link) => {
-        let href = link.getAttribute("href");
-        if (href && !href.startsWith("http") && !href.startsWith("#")) {
-          // Préfixe avec "/" pour un chemin absolu
-          link.setAttribute("href", "/" + href);
-        }
-      });
-
-      // Corriger l'image de la bannière si nécessaire
-      let bannerImg = document.querySelector("#header #banniere img");
-      if (bannerImg) {
-        let src = bannerImg.getAttribute("src");
-        if (src && !src.startsWith("http")) {
-          bannerImg.setAttribute("src", "/" + src);
-        }
-      }
     })
     .catch((error) => {
       console.error("Erreur lors du chargement du header:", error);
     });
 
   // Charger le footer depuis la racine du site
-  fetch("/footer/footer.html")
+  fetch(`${repoName}footer/footer.html`)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
